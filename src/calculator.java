f398a44f9e7507f4aeed85b12b88c20a74225ec3
safaalmaamari0-source <you@example.com>
2026-02-18 10:09:43 +0400

    factorial

--- src/calculator.java
+++ src/calculator.java
@@ -6,7 +6,19 @@ public class calculator {
 
             // code:
 
-            scanner.close();
+            System.out.println("Enter first number:");
+            int num1 = scanner.nextInt();
+            System.out.println("Enter second number:");
+            int num2 = scanner.nextInt();
+
+
+            int factorial = 1;
+            for (int i = num1; i >=1; i++){
+                factorial = factorial * i;
+            }
+            System.out.println("factorial of first number = " + factorial);
+
+
 
         }}
 
