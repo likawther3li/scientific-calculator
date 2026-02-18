850fe50938cf43e3c92621f7bebb276e91a3cfe4
Ahed Albarhi <iiihed7@gmail.com>
2026-02-18 10:09:56 +0400

    Square generation functionality

--- src/calculator.java
+++ src/calculator.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class calculator {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            // code:
 
-            scanner.close();
+        //3) Square generation functionality that takes one number as input.
+        System.out.println("Chose number to enter it: ");
+        int userNumber = scanner.nextInt();
+        int result = userNumber * userNumber;
+        System.out.println("Square = " + result);
+    }
+}
 
-        }}
+
+
+
 
