b16c9d4c2ba02e873e011b1f3f2d09854e19c96a
RimaAhmed1 <rimaaltarshi@gmail.com>
2026-02-18 10:08:53 +0400

    reformating the codes

--- src/calculator.java
+++ src/calculator.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class calculator {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            // code:
+        // code:
 
-            System.out.println("Enter a number: ");
-            int number=scanner.nextInt();
-            if(number<0){
-                int inputNumber=number*(-1);
-                System.out.println("The number in positive: "+inputNumber);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number < 0) {
+            int inputNumber = number * (-1);
+            System.out.println("The number in positive: " + inputNumber);
 
 
+        } else {
+            System.out.println("The number : " + number);
+        }
 
-            } else{
-                System.out.println("The number : "+number);
-            }
-
-        }}
+    }
+}
 
