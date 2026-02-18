756a1e6dd8310fe8337dbb201f474063b6f1ded9
RimaAhmed1 <rimaaltarshi@gmail.com>
2026-02-18 14:39:51 +0400

    a

--- src/calculator.java
+++ src/calculator.java
@@ -9,51 +9,32 @@ public class calculator {
         int num1 = scanner.nextInt();
         System.out.println("Enter second number:");
         int num2 = scanner.nextInt();
+        num1 = scanner.nextInt();
 
+        int power = 1;
+        for (int i = 1; i <= num2; i++) {
+            power = power * num1;
+        }
+        System.out.println("Power result: " + power);
 
+        int factorial = 1;
+        for (int i = num1; i >= 1; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("factorial of first number = " + factorial);
 
-//maryam
+        num1 = scanner.nextInt();
+        int result = num1 * num1;
+        System.out.println("Square = " + result);
 
-            int power = 1;
-            for (int i = 1; i<= num2; i++){
-                power = power * num1;
-            }
-            System.out.println("Power result: " +power );
-
-
-//safa
-
-            int factorial = 1;
-            for (int i = num1; i >=1; i++){
-                factorial = factorial * i;
-            }
-            System.out.println("factorial of first number = " + factorial);
-
-
-
-
-
-
-            //ahed
-
-             num1 = scanner.nextInt();
-             int result = num1 * num1;
-            System.out.println("Square = " + result);
-
-
-
-
-        //rima
-
-             num1 = scanner.nextInt();
-            if (num1 < 0) {
+        if (num1 < 0) {
 
             int inputNumber = num1 * (-1);
             System.out.println("The number in positive: " + inputNumber);
 
-            } else {
+        } else {
             System.out.println("The number : " + num1);
-            }
+        }
 
 
     }
