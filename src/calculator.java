f6c6daf6ac8b6fe35388b3060422f0fd1aab043d
mimi <mimi@gmail.com>
2026-02-18 10:11:23 +0400

    maryam part

--- src/calculator.java
+++ src/calculator.java
@@ -4,9 +4,23 @@ public class calculator {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            // code:
+
+
+            System.out.println("Enter first number:");
+            int num1 = scanner.nextInt();
+            System.out.println("Enter second number:");
+            int num2 = scanner.nextInt();
+
+
+
+            int power = 1;
+            for (int i = 1; i<= num2; i++){
+                power = power * num1;
+            }
+            System.out.println("Power result: " +power );
 
             scanner.close();
+
 
         }}
 
