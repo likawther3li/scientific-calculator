884e4f80c73f48aff782f5afdcb7ce298b1baa84
RimaAhmed1 <rimaaltarshi@gmail.com>
2026-02-18 10:06:13 +0400

    printing postive numbers

--- src/calculator.java
+++ src/calculator.java
@@ -6,7 +6,17 @@ public class calculator {
 
             // code:
 
-            scanner.close();
+            System.out.println("Enter a number: ");
+            int number=scanner.nextInt();
+            if(number<0){
+                int inputNumber=number*(-1);
+                System.out.println("The number in positive: "+inputNumber);
+
+
+
+            } else{
+                System.out.println("The number : "+number);
+            }
 
         }}
 
